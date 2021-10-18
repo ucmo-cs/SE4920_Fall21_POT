b0c051bd91a0f5c9538b1b2bf55079a5e64029b8
David Guest <dxg44670@ucmo.edu>
2021-10-18 14:17:49 -0500

    we are now running the mysql database online and the information connecting to that database has been changed.

--- Application/src/main/java/springboot/controller/HomeController.java
+++ Application/src/main/java/springboot/controller/HomeController.java
@@ -1,13 +1,20 @@
 package springboot.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 
-    @RequestMapping(value = "/")
-    public String index() {
-        return "App.js";
+
+    //Response Entity reepresents the whole HTTP response : status code, headers, and body
+
+    @GetMapping(value = "/")
+    public ResponseEntity<?> index() {
+        return new ResponseEntity<String>("Ok", HttpStatus.OK);
     }
 }
